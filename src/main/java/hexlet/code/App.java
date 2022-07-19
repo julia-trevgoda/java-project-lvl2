1df099264fbd69eb082f9ea2252c2098fdad9c34
Julia Trevgoda <julia.trevgoda@gmail.com>
2022-07-19 22:27:58 +0200

    diff flat json files

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,54 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+
+import java.io.File;
+import java.util.concurrent.Callable;
+
+
+@CommandLine.Command(
+        name = "gendiff",
+        mixinStandardHelpOptions = true,
+        version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference."
+)
+
+public class App implements Callable<Void> {
+
+    @CommandLine.Option(
+            names = {"-f", "--format"},
+            description = "output format [default: stylish]",
+            defaultValue = "stylish"
+    )
+    String format;
+
+    @CommandLine.Parameters(
+            index = "0",
+            paramLabel = "filepath1",
+            description = "path to first file"
+    )
+    private File file1;
+
+    @CommandLine.Parameters(
+            index = "1",
+            paramLabel = "filepath2",
+            description = "path to second file"
+    )
+    private File file2;
+
+    @Override
+    public Void call() throws Exception {
+
+        var diff = Differ.generate(file1, file2);
+        System.out.println(diff);
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            new CommandLine(new App()).execute(args);
+        } catch (Throwable throwable) {
+            System.out.println("Error " + throwable);
+        }
     }
 }
---
+++ src/main/java/hexlet/code/Differ.java
@@ -0,0 +1,84 @@
+package hexlet.code;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Differ {
+
+    public static String generate(File file1, File file2) throws IOException {
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Map<String, Object> json1
+                = objectMapper.readValue(Files.readString(file1.toPath().toAbsolutePath()), new TypeReference<TreeMap<String,Object>>(){});
+
+        Map<String, Object> json2
+                = objectMapper.readValue(Files.readString(file2.toPath().toAbsolutePath()), new TypeReference<TreeMap<String,Object>>(){});
+
+//        System.out.println(json1);
+//        System.out.println(json2);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("{").append("\n");
+
+        int size1 = json1.size();
+        int size2 = json2.size();
+
+        int iter1 = 0;
+        int iter2 = 0;
+
+        List<String> list1 = json1.keySet().stream().toList();
+        List<String> list2 = json2.keySet().stream().toList();
+
+        while (iter1 < size1 && iter2 < size2) {
+            String key1 =  list1.get(iter1);
+            String key2 =  list2.get(iter2);
+
+            if (key1.equals(key2)) {
+                if (json1.get(key1).equals(json2.get(key2))) {
+                    stringBuilder.append("  ").append(key1).append(": ")
+                            .append(json1.get(key1)).append("\n");
+                } else {
+                    stringBuilder.append("- ").append(key1).append(": ")
+                            .append(json1.get(key1)).append("\n");
+                    stringBuilder.append("+ ").append(key2).append(": ")
+                            .append(json2.get(key2)).append("\n");
+                }
+                iter1++;
+                iter2++;
+            } else if (key1.compareTo(key2) < 0) {
+                stringBuilder.append("- ").append(key1).append(": ")
+                        .append(json1.get(key1)).append("\n");
+                iter1++;
+            } else {
+                stringBuilder.append("+ ").append(key2).append(": ")
+                        .append(json2.get(key2)).append("\n");
+                iter2++;
+            }
+
+        }
+
+        while (iter1 < size1) {
+            stringBuilder.append("- ").append(list1.get(iter1)).append(": ")
+                    .append(json1.get(list1.get(iter1))).append("\n");
+            iter1++;
+        }
+
+        while (iter2 < size2) {
+            stringBuilder.append("+ ").append(list2.get(iter2)).append(": ")
+                    .append(json2.get(list2.get(iter2))).append("\n");
+            iter2++;
+        }
+
+        stringBuilder.append("}");
+
+        return stringBuilder.toString();
+    }
+}
