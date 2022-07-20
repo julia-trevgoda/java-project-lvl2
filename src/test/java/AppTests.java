a7fd87608029a9c7e24492f7a610e1437daa3e90
Julia Trevgoda <julia.trevgoda@gmail.com>
2022-07-20 13:04:16 +0200

    fix test reporter

---
+++ src/test/java/AppTests.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTests {
+    @Test
+    @DisplayName("'main' method works correctly")
+    void testMain() {
+
+    }
+}
